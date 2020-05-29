9e0184e5cb0919c89f5db1b8241a24235dba660d
azumi02468 <chemi10stry15@gmail.com>
2020-05-30 03:09:31 +0900

    Herokuセットアップ

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,15 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <video ref="myVideo" id="myVideo" width="400px" autoplay muted playsinline></video>
+    <video ref="theirVideo" id="theirVideo" width="400px" autoplay muted playsinline></video>
+    <div>
+      あなたのID：<input typp="text" id="myId" v-model="myId" readonly />
+    </div>
+    <div>
+      発信相手のID：<input type="text" id="theirId" v-model="theirId" />
+      <button id="makeCall" @click="onclick">発信</button>
+    </div>
   </div>
 </template>
 
@@ -35,8 +18,67 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      myId: "",
+      theirId: "",
+      localStream: ""
+    }
+  },
+  methods: {
+    // 発信処理
+    onclick: function(){
+      const theirID = this.theirId;
+      const mediaConnection = peer.call(theirID, this.localStream);
+      this.setEventListener(mediaConnection);
+    },
+    setEventListener: function(mediaConnection) {
+      mediaConnection.on('stream', stream => {
+        // video要素にカメラ映像をセットして再生
+        const videoElm = this.$refs.theirVideo;
+        videoElm.srcObject = stream;
+        videoElm.play();
+      });
+}
+  },
+  mounted: function(){
+    // カメラ映像取得
+    navigator.mediaDevices.getUserMedia({video: true, audio: true})
+      .then( stream => {
+      // 成功時にvideo要素にカメラ映像をセットし、再生
+      const videoElm = this.$refs.myVideo;
+      videoElm.srcObject = stream;
+      videoElm.play();
+      // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
+      this.localStream = stream;
+    }).catch( error => {
+      // 失敗時にはエラーログを出力
+      console.error('mediaDevice.getUserMedia() error:', error);
+      return;
+    });
+
+    //PeerID取得
+    peer.on('open', () => {
+        this.myId = peer.id;
+    });
+
+    //着信処理
+    peer.on('call', mediaConnection => {
+      mediaConnection.answer(this.localStream);
+      this.setEventListener(mediaConnection);
+    });
+
+    // エラー
+    peer.on('error', err => {
+      alert(err.message);
+    });
   }
 }
+
+// import
+import Peer from 'skyway-js';
+const peer = new Peer({key: '59093eb6-f094-476c-af62-ab068434ec23'});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
